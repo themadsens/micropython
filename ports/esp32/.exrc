be242d31e374393ff24d5282579f8e1b41703486
Flemming Madsen <fm@amplex.dk>
2021-09-28 19:32:11 +0200

    Start FM

---
+++ ports/esp32/.exrc
@@ -0,0 +1,1 @@
+" set tags+=/Users/fm/repos/esp-idfV4/tags
---
+++ ports/esp32/lfs-root/boot.py
@@ -0,0 +1,5 @@
+
+import sys,os,machine,uos,io,esp,esp,esp32
+from upysh import *
+
+print("Imported sys,os,machine,uos,io,esp,esp,esp32 for you")
